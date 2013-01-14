03099159824f2c2324b685245fc190a4fe939c84
Robby Colvin <geetarista@gmail.com>
2013-01-14 15:28:52 -0800

    Hacker News style updates

--- js/news.ycombinator.com.js
+++ js/news.ycombinator.com.js
@@ -1,7 +1,12 @@
 $(function() {
   // move form to header
-  $('span.yclinks').parent().find('form')
+  $('center center').find('form')
     .css({ 'color': 'black', 'position': 'absolute', 'right': '400px' })
     .prependTo('body');
-});
 
+  // I still like to see dead links/comments
+  $("span.dead").css("color", "red");
+
+  // my stuff is more prominent
+  $("font[color='#ff6600']").parent().parent().next().find("a").css("color", "#ff6600")
+});
